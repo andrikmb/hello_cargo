460d89f0a52fda90bdd905fe7aa8797342dcb442
Andrik Machado Batista <andrikmb@gmail.com>
2020-09-08 22:56:33 -0300

    build doc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate http;
+
+use http::{Request, Response, StatusCode};
+
 fn main() {
-    println!("Hello, world!");
+    let _request = Request::builder()
+        .uri("https://www.rust-lang.org/")
+        .header("User-Agent", "awesome/1.0")
+        .body(())
+        .unwrap();
+
+    let _response = Response::builder()
+        .status(StatusCode::MOVED_PERMANENTLY)
+        .header("Location", "https://www.rust-lang.org/install.html")
+        .body(())
+        .unwrap();
 }
